0070e8b9b9edcbbfb7d72fb4e07765dce7e52ef5
Krzysztof Synowiec <krzysztofsynowiec@krzysztofs-mbp.fritz.box>
2018-05-07 11:24:45 +0100

    Add new item functionality complete.

--- Mylist/MyListViewController.swift
+++ Mylist/MyListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MyListViewController: UITableViewController {
 
-    let itemArray = ["Buy Eggs", "Buy Tomatos", "Buy Bread"]
+    var itemArray = ["Buy Eggs", "Buy Tomatos", "Buy Bread"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,5 +42,34 @@ class MyListViewController: UITableViewController {
         
        tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item To Your List", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once the user clicks the Add Item button on our IUAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
